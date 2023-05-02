0bd2f0f1685db65f3d570081bfaef40946b85869
arkajitroy <arkajitroy11.official@gmail.com>
2023-05-02 22:12:42 +0530

    added : registration page

---
+++ frontend-server/src/components/pages/auth/registration/Registration.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import "./registration.css";
+
+const Registration = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleConfirmPasswordChange = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(
+      `Username: ${username}, Email: ${email}, Password: ${password}`
+    );
+    // Submit registration data to backend API here
+  };
+
+  return (
+    <div className="registration-page">
+      <h2 className="registration-page-headline">Create an Account</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="confirm-password">Confirm Password:</label>
+          <input
+            type="password"
+            id="confirm-password"
+            value={confirmPassword}
+            onChange={handleConfirmPasswordChange}
+            required
+          />
+        </div>
+        <button type="submit" className="register-button">
+          Register
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Registration;
