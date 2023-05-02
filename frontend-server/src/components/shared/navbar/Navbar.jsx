96d4015e711a245749c059c5a2ed8fa697359861
arkajitroy <arkajitroy11.official@gmail.com>
2023-05-02 22:37:41 +0530

    added: conditional rendering for navbar

--- frontend-server/src/components/shared/navbar/Navbar.jsx
+++ frontend-server/src/components/shared/navbar/Navbar.jsx
@@ -1,44 +1,59 @@
 import React from "react";
-import { NavLink, useNavigate } from "react-router-dom";
+import { NavLink, useNavigate, useLocation } from "react-router-dom";
 
 import "./navbar.css";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  console.log(location.pathname);
 
   const handleLoginButtonRedirect = () => {
     navigate("/auth/login");
   };
 
   return (
-    <nav className="navbar">
-      <ul className="navbar-list">
-        <li>
-          <NavLink to="/" activeClassName="active" className="navbar-link">
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/about" activeClassName="active" className="navbar-link">
-            About
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/contact"
-            activeClassName="active"
-            className="navbar-link"
-          >
-            Contact
-          </NavLink>
-        </li>
-      </ul>
-      <div className="navbar-login">
-        <button className="navbar-button" onClick={handleLoginButtonRedirect}>
-          Log In
-        </button>
-      </div>
-    </nav>
+    <>
+      {location.pathname === "/auth/login" ||
+      location.pathname === "/auth/register" ? null : (
+        <nav className="navbar">
+          <ul className="navbar-list">
+            <li>
+              <NavLink to="/" activeClassName="active" className="navbar-link">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/about"
+                activeClassName="active"
+                className="navbar-link"
+              >
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/contact"
+                activeClassName="active"
+                className="navbar-link"
+              >
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+          <div className="navbar-login">
+            <button
+              className="navbar-button"
+              onClick={handleLoginButtonRedirect}
+            >
+              Log In
+            </button>
+          </div>
+        </nav>
+      )}
+    </>
   );
 };
 
