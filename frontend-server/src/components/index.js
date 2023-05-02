d26dc6d822ce2b727f7f3c2ad1e8995d892753c0
arkajitroy <arkajitroy11.official@gmail.com>
2023-05-02 21:29:32 +0530

    added: page components and index files

---
+++ frontend-server/src/components/index.js
@@ -0,0 +1,7 @@
+// PAGES COMPONENTS
+export { default as HomePage } from "./pages/Home.jsx";
+export { default as AboutPage } from "./pages/About.jsx";
+export { default as ContactPage } from "./pages/Contact.jsx";
+
+// SHARED COMPONENTS
+export { default as Navbar } from "./shared/navbar/Navbar.jsx";
