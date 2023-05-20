6ab02f08f22e1063d7dae064f0f96e05a5099cdb
arkajitroy <arkajitroy11.official@gmail.com>
2023-05-20 10:58:51 +0530

    added: jwt token generate function

---
+++ src/utils/generateJWT.js
@@ -0,0 +1,9 @@
+const jwt = require("jsonwebtoken");
+
+function generateJWT(id) {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+}
+
+module.exports = { generateJWT };
